a72e362b33d7d5e5d92b09114097b4663a30c0dc
Piyush Waghela <piyush.waghela15514@sakec.ac.in>
2024-11-15 00:02:47 +0530

    Added shadcn ui, react-router-dom and completed the ui of signup and sign in

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,17 @@
 import './App.css'
+import { Signin } from './components/Signin'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Signup } from './components/Signup';
 
 function App() {
  
   return (
-    <>
-      hii there
-    </>
+    <Router>
+     <Routes>
+      <Route path='/sign-in' element={<Signin/>} />
+      <Route path='/sign-up' element={<Signup/>} />
+     </Routes>
+    </Router>
   )
 }
 
---
+++ frontend/src/components/Signin.tsx
@@ -0,0 +1,71 @@
+import { Eye, EyeOff, Globe } from "lucide-react";
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
+import { useState } from "react";
+import { Button } from "./ui/button";
+import { Link } from "react-router-dom";
+
+export const Signin = () => {
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+  return (
+    <div className="min-h-screen bg-gradient-to-b from-blue-200 to-blue-30000 flex flex-col justify-center items-center px-4">
+      <div className="max-w-md bg-white w-full rounded-lg shadow-md p-8">
+        <div className="flex justify-center mb-8">
+          <Globe className="size-12 text-blue-600" />
+        </div>
+        <h2 className="text-3xl font-bold text-center">
+          Login to Global Trade Connect
+        </h2>
+        <form className="space-y-6 ">
+          <div>
+            <div className="pb-2">
+              <Label htmlFor="email">Email</Label>
+            </div>
+            <Input
+              id="email"
+              type="email"
+              placeholder="vedika@gmail.com"
+              required
+            />
+          </div>
+          <div>
+            <Label htmlFor="password">Password</Label>
+            <div className="relative gap-2 pt-2">
+              <Input
+                id="password"
+                type={showPassword ? "text" : "password"}
+                placeholder="********"
+                required
+              />
+              <button
+                type="button"
+                className="absolute inset-y-0 right-0 pr-3 flex items-center"
+                onClick={() => setShowPassword(!showPassword)}
+              >
+                {showPassword ? (
+                  <EyeOff className="size-5 text-blue-400" />
+                ) : (
+                  <Eye className="size-5 text-blue-400" />
+                )}
+              </button>
+            </div>
+          </div>
+          <Button type="submit" className="w-full">
+            Log In
+          </Button>
+        </form>
+        <div className="mt-6 text-center">
+          <Link to="" className="text-blue-600 hover:underline">
+            Forgot password
+          </Link>
+        </div>
+      </div>
+      <p className="mt-8 text-center text-sm text-gray-600">
+        Don{"' "} have an account?{" "}
+        <Link to="#" className="font-medium text-blue-600 hover:underline">
+          Sign up
+        </Link>
+      </p>
+    </div>
+  );
+};
